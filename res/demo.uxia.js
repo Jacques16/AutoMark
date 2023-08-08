4478fe97311a8c5151277462a00e106031800854
Jacques16 <79564547+Jacques16@users.noreply.github.com>
2023-08-09 01:34:27 +0300

    Update demo.uxia.js

--- res/demo.uxia.js
+++ res/demo.uxia.js
@@ -2,7 +2,7 @@
  *
  * demo.uxia.js v0.0.1
  * (c) copyright 2023-2024 RAKOTOMAMONJY Jacques Lyard
- * all right reserved
+ * released into MIT License 
  *
  */
 function _createForOfIteratorHelperLoose(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (it) return (it = it.call(o)).next.bind(it); if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; return function () { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
