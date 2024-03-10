9aaa454107d73c0342703934ea6d9e8f448cccc3
jacqueslyard <79564547+Jacques16@users.noreply.github.com>
2024-03-10 23:39:14 +0300

    Create data.router.js

---
+++ example/data.router.js
@@ -0,0 +1,23 @@
+var Page1 = {
+ divPage1: {
+  h3: "1. Installer une application Node.js sur votre appareil",
+   'p.innerHTML': "Recherchez dans le <b>Google Play</b> Store des applications qui permettent d'installer et d'exécuter Node.js sur votre appareil Android. Des applications telles que <b>Termux</b>, <b>GNURoot Debian</b>, ou encore <b>Termux:API</b> sont populaires pour ce type d'utilisation. Téléchargez et installez l'application de votre choix."
+ }
+},
+Page2 = {
+ divPage2: {
+ h3: "2. Installer Node.js dans l'application",
+  'p.innerHTML': "Une fois que vous avez installé l'application, ouvrez-la et suivez les instructions pour installer <b>Node.js</b> à l'intérieur de celle-ci. Certaines applications peuvent inclure <b>Node.js</b> dans leur système de gestion de paquets, tandis que d'autres peuvent nécessiter un téléchargement et une installation manuels."
+ }
+}, Page3 = {
+ divPage3: {
+  h3: "3. Éditer et exécuter vos scripts ",
+   'p.innerHTML': "Une fois que <b>Node.js</b> est installé sur votre appareil, vous pouvez utiliser l'éditeur de code intégré à l'application ou une autre application d'édition de code tierce pour écrire vos scripts <b>Node.js</b>. Ensuite, vous pouvez exécuter ces scripts directement à partir de l'application."
+ }
+},
+Page4 = {
+ divPage4: {
+ h3: "4. Gérer vos projets",
+ 'p.innerHTML': "Vous pouvez organiser vos projets <b>Node.js</b> sur votre appareil Android en créant des dossiers pour les différents scripts et applications que vous développez. Assurez-vous de sauvegarder régulièrement votre code pour éviter toute perte de données."
+ }
+}
